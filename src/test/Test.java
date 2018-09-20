a670bd7c9b8954a553c6bbb1667924092d196afc
Chasal <lyz00054@163.com>
2018-09-21 00:15:04 +0800

    init project

---
+++ src/test/Test.java
@@ -0,0 +1,5 @@
+package test;
+
+public interface Test {
+    final static int a = 1;
+}
---
+++ src/test/TestImpl.java
@@ -0,0 +1,9 @@
+package test;
+
+public class TestImpl implements Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(TestImpl.a);
+    }
+}
